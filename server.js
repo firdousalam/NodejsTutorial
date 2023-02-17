95c47df434deac4fd35392c2dc5de1b4a6964f6a
firdous alam <firdous.alam2058@gmail.com>
2023-02-17 23:54:20 +0530

    code changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+console.log("mmmmmmmmmmm");
 app.get('/', (req, res) => {
   res.send('Hello World! '+req.protocol + '://' + req.get('host') + req.originalUrl);
 })
