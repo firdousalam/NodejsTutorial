85862b60f17597ac07002bc16df5b7cf8c52891d
firdous alam <firdous.alam2058@gmail.com>
2023-02-20 15:50:04 +0530

    code new changes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 console.log("mmmmmmmmmmm");
 app.get('/', (req, res) => {
+  console.log(req);
   var pathname = url.parse(req.url).pathname;
   console.log("Request for " + pathname + " received.");
   console.log(req.url);
