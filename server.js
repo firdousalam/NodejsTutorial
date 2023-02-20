2294ccabc9289a1b6900999be6d07c984deebc40
firdous alam <firdous.alam2058@gmail.com>
2023-02-20 16:14:20 +0530

    new code deployment

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express()
 const port = 3000
-console.log("new deployment");
+console.log("new deployment for new code");
 app.get('/', (req, res) => {
+  console.log(req.headers);
+
   var host = req.get('host');
   var origin = req.get('origin');
   var Hhost = req.headers.host;
 var Horigin = req.headers.origin;
+const { pathname } = req?._parsedUrl || {};
+console.log(pathname);
   res.send('Hello World! '+host+" origin "+origin+" hHost"+Hhost+" hOrigin "+Horigin);
 })
 
