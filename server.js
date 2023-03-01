feae290f09def34a3ffc6c6d3fceef171a71bd43
firdous alam <firdous.alam2058@gmail.com>
2023-03-01 15:27:36 +0530

    new changes

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const app = express()
 const port = 3000
 console.log("new deployment for new code");
 app.get('/', (req, res) => {
-  console.log(req.headers);
+  console.log(req.get('User-Agent'));
 
   var host = req.get('host');
   var origin = req.get('origin');
   var Hhost = req.headers.host;
 var Horigin = req.headers.origin;
 const { pathname } = req?._parsedUrl || {};
-console.log(pathname);
-  res.send('Hello World! '+host+" origin "+origin+" hHost"+Hhost+" hOrigin "+Horigin);
+var Url = req.protocol + '://' + req.get('host') + req.originalUrl;
+console.log(Url);
+//sconsole.log(pathname);
+const clientUrl = req.header('Referer');
+console.log(clientUrl);
+  res.send('Hello World! '+Url+"client browser Url"+clientUrl);
 })
 
 app.listen(port, () => {
