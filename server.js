f7be77a71eae04fd11cac5e189aa008523e4e673
firdous alam <firdous.alam2058@gmail.com>
2023-05-30 00:30:04 +0530

    Nodejs Get and post into MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express()
 const port = 5000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-//mongodb://TechnophileFirdous:Technophile@1234@ac-k5g9okb-shard-00-00.kzuwf7d.mongodb.net:27017,ac-k5g9okb-shard-00-01.kzuwf7d.mongodb.net:27017,ac-k5g9okb-shard-00-02.kzuwf7d.mongodb.net:27017/?ssl=true&replicaSet=atlas-xjy27e-shard-0&authSource=admin&retryWrites=true&w=majority
-mongoose.connect("mongodb://TechnophileFirdous:Technophile1234@ac-k5g9okb-shard-00-00.kzuwf7d.mongodb.net:27017,ac-k5g9okb-shard-00-01.kzuwf7d.mongodb.net:27017,ac-k5g9okb-shard-00-02.kzuwf7d.mongodb.net:27017/?ssl=true&replicaSet=atlas-xjy27e-shard-0&authSource=admin&retryWrites=true&w=majority"
+console.log("my Database config",process.env.DBCONFIG)
+mongoose.connect(process.env.DBCONFIG
 ,
   {
     useNewUrlParser: true,
@@ -18,10 +19,12 @@ db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("Connected successfully");
 });
+
 const userModel = require("./model/userSchema");
-app.post("/add_user", async (request, response) => {
+app.post("/addUser", async (request, response) => {
+
   let insertData = {"name" : request.body.name,
-"age" : request.body.age};
+                    "age" : request.body.age};
   const user = new userModel(request.body);
   console.log("app hit");
   try {
@@ -31,6 +34,27 @@ app.post("/add_user", async (request, response) => {
     response.status(500).send(error);
   }
 });
+
+
+app.get('/getUser',function(req,res){
+  userModel.find({}).then((list)=>{
+    res.send(list);
+  }).catch((err)=>{
+      res.status(500).send(error);
+  })
+})
+
+
+app.get('/user/:listId', function (req, res) {
+  userModel.find({"_id":req.params.listId}).then((list)=>{
+      res.send(list);
+  }).catch((err)=>{
+      res.send(err);
+  })
+});
+
+
+
 /*
 
 
@@ -38,22 +62,6 @@ const loginRoute = require('./router/loginRoute')
 const userRoute = require('./router/userRoute');
 const userModel = require("./model/userSchema");
 //const mongoConfig = require('./config/MongoDBConfig');
-
-
-// database connection
-//mongodb://MeanYoutube:MeanYoutube1234@ac-gnqfovj-shard-00-00.rxlvclw.mongodb.net:27017,ac-gnqfovj-shard-00-01.rxlvclw.mongodb.net:27017,ac-gnqfovj-shard-00-02.rxlvclw.mongodb.net:27017/?ssl=true&replicaSet=atlas-y84hfx-shard-0&authSource=admin&retryWrites=true&w=majority"
-mongoose.connect("mongodb://MeanYoutube:MeanYoutube1234@ac-gnqfovj-shard-00-00.rxlvclw.mongodb.net:27017,ac-gnqfovj-shard-00-01.rxlvclw.mongodb.net:27017,ac-gnqfovj-shard-00-02.rxlvclw.mongodb.net:27017/?ssl=true&replicaSet=atlas-y84hfx-shard-0&authSource=admin&retryWrites=true&w=majority"
-,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
 
 
 
